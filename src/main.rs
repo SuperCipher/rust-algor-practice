a0d10f71283f180595523c1079923c1a8149eb09
Napat <napat.charuphant1@gmail.com>
2022-06-03 21:13:43 +0000

    feat: pair combination

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,20 @@
-// Rust functional style
+// fn main() {
+//   let collectible = (1..6).collect::<Vec<i32>>().into_iter().map(|x|x).collect::<Vec<i32>>();
+//   println!("{:?}",collectible)
+//   // (1..6).collect::<Vec<i32>>().iter().map(|x|(1..*x+1).collect::<Vec<i32>>().iter().map(|y| println!("({},{})",x,y)));
+// }
+
+
 fn main() {
-  let i: Vec<i32>  = (1..6).collect::<Vec<i32>>();
-  let i_iter:std::slice::Iter<i32> = i.iter();
-  println!("Collected (0..10) into: {:?}", i);
-  let j: Vec<i32>  = (1..6).collect::<Vec<i32>>();
-  let j_iter:std::slice::Iter<i32> = j.iter();
-  println!("i_iter {:?}",i_iter);
 
-  // let two_d_vec :  Vec<Vec<(&i32,&i32)>> = i_iter.map(|i| j_iter.map(|j| (j,j)).collect() ).collect();
-  let two_d_vec :  Vec<Vec<(&i32,&i32)>> = i_iter.clone().map(|i: &i32| j_iter.clone().map(|j: &i32| (j,j)).collect() ).collect();
-  println!("two_d_vec {:?}",two_d_vec);
-  // println!("i {:?}",i);
-  println!("i_iter {:?}",i_iter);
+  let mut result : Vec<(&i32,&i32)> = vec![];
 
+  // (1..6).for_each(|x| println!("({})",x));
+  // let fm = (1..6).flat_map(|x| (1..=x)).collect::<Vec<_>>();
 
-  // let mut xs = vec![4i32, 2, 3];
-  // println!("Initial vector: {:?}", xs);
+  let fm = (1..6).flat_map(|x| (1..=x-1).map(move |y| (y, x))).collect::<Vec<_>>();
+  println!("{:?}",fm)
 
+  // (1..6).collect::<Vec<i32>>().iter().for_each(move |x|(1..*x+1).collect::<Vec<i32>>      ().iter().for_each( move |y| result.push((x,y))));
 
-  
-  // xs.iter()
-  //   .map(|x| 0xff)
-  //   .collect();
 }
--- src/placeholder.rs
+++ src/placeholder.rs
@@ -1,19 +1,18 @@
-// Rust functional style
-fn main() {
-  let i: Vec<i32>  = (1..6).collect::<Vec<i32>>();
-  let i_iter:std::slice::Iter<i32> = i.iter();
-  println!("Collected (0..10) into: {:?}", i);
-  let j: Vec<i32>  = (1..6).collect::<Vec<i32>>();
-  let j_iter:std::slice::Iter<i32> = j.iter();
-
-  let two_d_vec :  Iterator<Item=(&i32, &i32)> = i_iter.map(|i| (i,i)).collect();
-  println!("two_d_vec {:?}",two_d_vec);
-  // let mut xs = vec![4i32, 2, 3];
-  // println!("Initial vector: {:?}", xs);
-
-
+// // Rust functional style
+// fn main() {
+//   let i: Vec<i32>  = (1..4).collect::<Vec<i32>>();
+//   let i_iter:std::slice::Iter<i32> = i.iter();
+//   println!("i_iter {:?}",i_iter);
+//   let j: Vec<i32>  = (1..3).collect::<Vec<i32>>();
+//   let j_iter:std::slice::Iter<i32> = j.iter();
+//   println!("j_iter {:?}",j_iter);
   
-  // xs.iter()
-  //   .map(|x| 0xff)
-  //   .collect();
-}
+//   // let two_d_vec :  Vec<Vec<(&i32,&i32)>> = i_iter.clone().map(|i: &i32| j_iter.clone().map(|j: &i32| (i,j)).collect() ).collect();
+
+//   let two_d_vec :  Vec<Vec<(&i32,&i32)>> = i_iter.clone().map(|i: &i32| (1..3).collect::<Vec<i32>>().iter().clone().map(|j: &i32| (i,j)).collect() ).collect();
+  
+//   println!("two_d_vec {:?}",two_d_vec);
+//   // println!("i {:?}",i);
+//   println!("i_iter {:?}",i_iter);
+
+// }
